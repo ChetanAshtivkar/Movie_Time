60c1655ecad168acfc2bb9344c56755e3aeb08b9
Chetan Ashtivkar <chetan.ashtivkar@gmail.com>
2020-03-04 23:54:37 +0530

    Added necessary permissions, libraries and classes to the project

---
+++ app/src/main/java/com/chetan/movietime/common/NavigationUtils.kt
@@ -0,0 +1,13 @@
+package com.rebel.demo.common
+
+import androidx.lifecycle.MutableLiveData
+
+/**
+ * Created by Chetan on 2019-10-13.
+ */
+
+/**
+ * Mutable live data of type Navigator.
+ * This is to be observed on each activity
+ */
+class NavigationUtils : MutableLiveData<Navigator>()
